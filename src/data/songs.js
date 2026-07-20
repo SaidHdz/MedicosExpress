d7a19615fce4f390e8261baf3f29c5d33e757dee
Said <SaidDino01@gmail.com>
2026-07-19 22:42:09 -0500

    feat: reemplazar links de busqueda por links directos de spotify en todas las canciones

--- src/data/songs.js
+++ src/data/songs.js
@@ -26,7 +26,7 @@ export const initialSongs = [
     artist: "Kyotto",
     cover: deidadCover,
     note: "ven y vamos a querernos como si na' de esto doliera",
-    spotifyUrl: "https://open.spotify.com/search/Kyotto%20Cosas%20Que%20Jamas%20Diria%20a%20Nadie",
+    spotifyUrl: "https://open.spotify.com/track/1ixuSijWiqq5ghkhqRmqY5?si=81176aabe1264669",
     type: "single",
     date: '2026-07-19T00:00:00Z'
   },
@@ -36,7 +36,7 @@ export const initialSongs = [
     artist: "Álvaro Díaz",
     cover: felicilandiaCover,
     note: "no se que dia e' ya va a anochece' y no quiero hacer nada mas que mirar tu cara",
-    spotifyUrl: "https://open.spotify.com/search/Alvaro%20Diaz%20Gatillera",
+    spotifyUrl: "https://open.spotify.com/track/7lvEdc8T7o6mYzP94qHaqf?si=cd1a7e38fcdd4660",
     type: "single",
     date: '2026-07-19T00:00:00Z'
   },
@@ -46,7 +46,7 @@ export const initialSongs = [
     artist: "Álvaro Díaz",
     cover: sayonaraCover,
     note: "es que tu na ma, me dejas sin poderes",
-    spotifyUrl: "https://open.spotify.com/search/Alvaro%20Diaz%20SIN%20PODERES",
+    spotifyUrl: "https://open.spotify.com/track/1pA3sCCfztmO0zLHcbzutW?si=539ad7ec74f34d9e",
     type: "single",
     date: '2026-07-19T00:00:00Z'
   },
@@ -56,7 +56,7 @@ export const initialSongs = [
     artist: "Rels B",
     cover: tuVasSinCover,
     note: "yo quiero todo, quiero que tu te enamores, baby, yo te quiero solo pa mi",
-    spotifyUrl: "https://open.spotify.com/search/Rels%20B%20TU%20VAS%20SIN",
+    spotifyUrl: "https://open.spotify.com/track/6uiQ0DqbXTBc8tl0AUKZkR?si=3c9fc5e8007141e7",
     type: "single",
     date: '2026-07-19T00:00:00Z'
   },
@@ -66,7 +66,7 @@ export const initialSongs = [
     artist: "Big Sempa",
     cover: loteCover,
     note: "Porque somos raros y nos entendemos, Ya no importa nada porque nos tenemos",
-    spotifyUrl: "https://open.spotify.com/search/Big%20Sempa%20La%20Guera",
+    spotifyUrl: "https://open.spotify.com/track/0KkgjZ6v05cSNo7W6IsZqi?si=b975ebf3401243b5",
     type: "single",
     date: '2026-07-19T00:00:00Z'
   },
@@ -76,7 +76,7 @@ export const initialSongs = [
     artist: "Artista",
     cover: aceleraaCover,
     note: "Ma, tú ere mi estilo, tú mi type beat",
-    spotifyUrl: "https://open.spotify.com/search/ACELERAA%C3%81",
+    spotifyUrl: "https://open.spotify.com/track/2BEJp2meUuyLN8TxU3bZQn?si=5d9cfc06645e415e",
     type: "single",
     date: '2026-07-19T00:00:00Z'
   },
@@ -86,7 +86,7 @@ export const initialSongs = [
     artist: "NSQK",
     cover: sisifoCover,
     note: "por que eres tu, no quiero a nadie mas",
-    spotifyUrl: "https://open.spotify.com/search/NSQK%20NADIE%20MAS!",
+    spotifyUrl: "https://open.spotify.com/track/0HWyOfh3Q08UreN155KmRZ?si=0f26b4fe40f5491a",
     type: "single",
     date: '2026-07-19T00:00:00Z'
   },
@@ -96,7 +96,7 @@ export const initialSongs = [
     artist: "NSQK",
     cover: royCover,
     note: "Que solo quiero saber Si una mariposa viste en tu ventana Cómo te pediste el café por la mañana Y si en tu mente estuve",
-    spotifyUrl: "https://open.spotify.com/search/NSQK%20Si%20en%20tu%20mente%20estuve",
+    spotifyUrl: "https://open.spotify.com/track/0QYSECtjVCysOVy77g31eS?si=7bc0ab416ce34551",
     type: "single",
     date: '2026-07-19T00:00:00Z'
   },
@@ -112,19 +112,19 @@ export const initialSongs = [
         id: "bimel",
         title: "BIMEL.",
         note: "Tu te ve' bien, pero conmigo te veria' mejor", 
-        spotifyUrl: "https://open.spotify.com/search/Alvaro%20Diaz%20BIMEL"
+        spotifyUrl: "https://open.spotify.com/track/2KpWqlm7ceW9erFeHFvCNP?si=71fcfba1150a4634"
       },
       {
         id: "spacexxx",
         title: "SPACEXXX.",
         note: "pensando en ti pensando en ti to el dia, toa despeina' con una t-shirt mia", 
-        spotifyUrl: "https://open.spotify.com/search/Alvaro%20Diaz%20SPACEXXX"
+        spotifyUrl: "https://open.spotify.com/track/3fQc7Ca6xCOeNEKvtuRhUN?si=b354b7ea48f942b7"
       },
       {
         id: "inarow62",
         title: "INAROW62.",
         note: "Tus ojitos chinos, mami, siempre quiero, Si, eso es lo que quiero", 
-        spotifyUrl: "https://open.spotify.com/search/Alvaro%20Diaz%20INAROW62"
+        spotifyUrl: "https://open.spotify.com/track/5Yhti6baaZnDyzehjTuAwZ?si=5fd36777e4cc4198"
       }
     ]
   }
